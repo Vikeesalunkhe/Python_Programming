78700759d2e17f4375c63604d5d6def99832aeef
vikee salunkhe <vikeesalunkhe2002@gmail.com>
2023-04-06 22:35:44 +0530

    python assingment

---
+++ Python-Assingment/while-loop Assingment 2/1_program.py
@@ -0,0 +1,16 @@
+# program 1:write a program to print a table of 2
+
+"""number=2
+i = 1;
+while i <= 10:
+
+    print( i * 2)
+    i = i + 1"""
+
+number = 3
+i = 1;
+while i<= 10:
+
+    print(i*3)
+     
+    i = i + 1
