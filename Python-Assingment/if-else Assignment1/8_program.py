57fbdddbbab64a56c6d302693f0ef11c9721e27d
vikee salunkhe <vikeesalunkhe2002@gmail.com>
2023-04-08 23:45:19 +0530

    python assingment

---
+++ Python-Assingment/if-else Assignment1/8_program.py
@@ -0,0 +1,21 @@
+# program 8:write a program to check day no. (1-7) and print the corresponding day of week
+
+num = int(input("Enter your number :"))
+
+if num==1:
+    print("Monday")
+elif num==2:
+    print("Tuesday")
+elif num==3:
+    print("Wensday")
+elif num==4:
+    print("Thursday")
+elif num==5:
+    print("Fraiday")
+elif num==6:
+    print("Saturday")
+elif num==7:
+    print("Sunday")
+else:
+    print("invalid number")
+
